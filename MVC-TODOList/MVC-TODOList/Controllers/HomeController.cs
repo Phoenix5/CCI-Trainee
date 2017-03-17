19902eda7321c0c78c934521f9d27f32faa54887
Phoenix5 <mashelkarprathamesh@gmail.com>
2017-03-17 09:28:53 +0530

    mvc-todo list

---
+++ MVC-TODOList/MVC-TODOList/Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using MVC_TODOList.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_TODOList.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AddItem(ToDoModel mod)
+        {
+            if (mod.DeleteKey == 0)
+                ToDoModel.todos.Add(ToDoModel.todos.Keys.Any() ? ToDoModel.todos.Keys.Max() + 1 : 1, mod.Item);
+            else
+            {
+                ToDoModel.todos[mod.DeleteKey]= mod.Item;
+                mod.DeleteKey = 0;
+            }
+
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult EditItem(ToDoModel mod)
+        {
+           // ToDoModel.todos.Remove(mod.DeleteKey);
+           // ToDoModel.todos.Add(mod.DeleteKey,mod.Item);
+            return View("Index");
+        }
+
+        public ActionResult RemoveItem(ToDoModel mod)
+        {
+            ToDoModel.todos.Remove(mod.DeleteKey);
+            mod.DeleteKey = 0;
+            return RedirectToAction("Index");
+        }
+
+
+
+
+
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ MVC-TODOList/MVC-TODOList/Models/ToDoModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_TODOList.Models
+{
+    public class ToDoModel
+    {
+        public int check=222;
+        public string Item { get; set; }
+        public int DeleteKey { get; set; }
+        public static Dictionary<int,string> todos { get; set; }
+
+        static ToDoModel()
+        {
+            todos = new Dictionary<int,string>();
+        }
+    }
+}
