29f86eeba9735fe2a811d2190af05236dcd26e24
Phoenix5 <mashelkarprathamesh@gmail.com>
2017-03-19 19:55:26 +0530

    employee reviw build 1

---
+++ Employee-Summary-build1/Employee-Summary/Controllers/EmployeeReviewController.cs
@@ -0,0 +1,93 @@
+﻿using Employee_Summary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Employee_Summary.Controllers
+{
+
+    public static class iList {
+
+        /*  Dev_Skills d = new Dev_Skills()
+        {   CodingSkills = Dev_Rating.NeedsImprovement,
+            QualityAssurance = Dev_Rating.MeetsExpectations,
+            TimeLogging = Dev_Rating.AboveExpectations,
+            TroubleShootingSkills = Dev_Rating.Outstanding
+        };
+        Tech_skills t = new Tech_skills()
+        {
+            TFS=Tech_Rating.Beginner,
+            GIT=Tech_Rating.Expert,
+            Ruby=Tech_Rating.Intermediate,
+            ASPDotNETCORE=Tech_Rating.Proficient,
+            AngularJS=Tech_Rating.Expert,
+            CSharp=Tech_Rating.Expert
+
+        };*/
+        //  private int i = 2;
+        public static IList<Summary> EList = new List<Summary>();
+        /*  {new Summary {Id=1 ,Name="Prathamesh",Designation=Designations.JuniorDeveloper,Dskill=new Dev_Skills()
+          {
+              CodingSkills = Dev_Rating.NeedsImprovement,
+              QualityAssurance = Dev_Rating.MeetsExpectations,
+              TimeLogging = Dev_Rating.AboveExpectations,
+              TroubleShootingSkills = Dev_Rating.Outstanding
+          },Tskill=new Tech_skills()
+          {
+              TFS=Tech_Rating.Beginner,
+              GIT=Tech_Rating.Expert,
+              Ruby=Tech_Rating.Intermediate,
+              ASPDotNETCORE=Tech_Rating.Proficient,
+              AngularJS=Tech_Rating.Expert,
+              CSharp=Tech_Rating.Expert
+          }
+          }
+          };*/
+
+    }
+    public class EmployeeReviewController : Controller
+    {
+      
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Category(Summary summary1)
+        {
+           // i++;
+           foreach(var item in iList.EList)
+            {
+                if (item.Designation == summary1.Designation && item.Name == summary1.Name)
+                    return View("Summary",item);
+            }
+            return View(summary1);
+        }
+
+
+        public ActionResult DevSkills(Summary summary2)
+        {
+            summary2.set = true;
+            return View(summary2);
+        }
+
+        public ActionResult TechSkills(Summary summary3)
+        {
+            summary3.set = true;
+            //summary3.Tskill.Ruby = Tech_Rating.Intermediate;
+            return View(summary3);
+        }
+
+        public ActionResult Summary(Summary summary4)
+        {  // if (summary3.set == 1)
+            iList.EList.Add(new Summary(summary4.Name,summary4.Designation,summary4.Dskill,summary4.Tskill));
+                return View(summary4);
+           // else
+           //     return RedirectToAction("Index","EmployeeReview");
+
+        }
+        
+    }
+}
---
+++ Employee-Summary-build1/Employee-Summary/Models/Summary.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Employee_Summary.Models
+{
+    public class Summary
+    {
+        public int Id { get; set; } = 0;
+        public string Name { get; set; }
+        public Designations Designation { get; set; }
+        public Dev_Skills Dskill { get; set; }
+        public Tech_skills Tskill { get; set; }
+
+        public bool set { get; set; } = false;
+        public Summary()
+        {
+            Id = Id + 1;
+            Name = "Not Entered";
+        }
+        public Summary(string name, Designations designation, Dev_Skills dskill, Tech_skills tskill)
+        {
+            Id = Id + 1;
+            this.Name = name;
+            this.Designation = designation;
+            this.Dskill = new Dev_Skills(dskill.CodingSkills,dskill.QualityAssurance,dskill.TimeLogging,dskill.TroubleShootingSkills);
+            this.Tskill = new Tech_skills(tskill.AngularJS,tskill.ASPDotNETCORE,tskill.CSharp,tskill.GIT,tskill.Ruby,tskill.TFS);
+        }
+    }
+    public class Dev_Skills
+    {
+        public Dev_Rating CodingSkills{get;set;}
+        public Dev_Rating TroubleShootingSkills {get;set;}
+        public Dev_Rating QualityAssurance {get;set;}
+        public Dev_Rating TimeLogging {get;set;}
+        public Dev_Skills() { }
+        public Dev_Skills(Dev_Rating c, Dev_Rating q, Dev_Rating t, Dev_Rating tr)
+        {
+            this.CodingSkills = c;
+            this.QualityAssurance = q;
+            this.TimeLogging = t;
+            this.TroubleShootingSkills = tr;
+        }
+    }
+    public class Tech_skills
+    {
+        public Tech_Rating TFS{ get; set; }
+        public Tech_Rating GIT{get;set;}
+        public Tech_Rating CSharp{get;set;}
+        public Tech_Rating ASPDotNETCORE{get;set;}
+        public Tech_Rating Ruby{get;set;}
+        public Tech_Rating AngularJS {get;set;}
+        public Tech_skills() { }
+        public Tech_skills(Tech_Rating aj,Tech_Rating a,Tech_Rating c,Tech_Rating g,Tech_Rating r,Tech_Rating t)
+        {
+            this.AngularJS = aj;
+            this.ASPDotNETCORE = a;
+            this.CSharp = c;
+            this.GIT = g;
+            this.Ruby = r;
+            this.TFS = t;
+        }
+    }
+
+    public enum Dev_Rating { NotRated,Unsatisfactory, NeedsImprovement, MeetsExpectations, AboveExpectations, Outstanding }
+    public enum Tech_Rating { NotRated,Beginner, Intermediate, Proficient, Expert }
+    public enum Designations { NotGiven,JuniorDeveloper,Developer,SeniorDeveloper,Manager}
+}
