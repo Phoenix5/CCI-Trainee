4ecf9eeff7835668e97b116d0e5116039fb6307c
Phoenix5 <mashelkarprathamesh@gmail.com>
2017-03-27 16:52:29 +0530

    Final ToDo List using web api's

---
+++ WebAPI_ToDo_final/WebAPI_Basic/API/ToDoAPIController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web;
+
+using WebAPI_Basic.Models;
+
+namespace WebAPI_Basic.API
+{
+    public class ToDoAPIController : ApiController
+    {
+        TaskContext db = new TaskContext();
+
+        // GET: api/CarDetails
+        [HttpGet]
+        public IEnumerable<Tasks> GetToDoList()
+        {
+           
+            List<Tasks> li = db.task.ToList(); 
+
+
+            return li;
+        }
+
+        // GET: api/CarDetails/5
+        [HttpGet]
+        public IEnumerable<Tasks> Get(int id)
+        {
+            
+            List<Tasks> li = db.task.ToList();
+            
+
+            return li.Where(n=>n.Id==id);
+        }
+
+        // POST: api/CarDetails
+        [HttpPost]
+        public int PostToDo([FromBody]Tasks cs)
+        {
+            Tasks t = new Tasks() { TaskName=cs.TaskName};
+            db.task.Add(t);
+            db.SaveChanges();
+            return 1;
+         //   RedirectToRoute("Default",new { });
+        }
+
+        // PUT: api/CarDetails/5
+        [HttpPut]
+        public int PutToDo(int id, [FromBody]string value)
+        {
+            var temp = db.task.SingleOrDefault(m => m.Id == id);
+            temp.TaskName = value;
+            db.SaveChanges();
+            return 1;
+        }
+
+        // DELETE: api/CarDetails/5
+        [HttpDelete]
+        public int DeleteToDo(int id)
+        {
+            var itemToRemove = db.task.SingleOrDefault(x => x.Id == id); //returns a single item.
+
+            if (itemToRemove != null)
+            {
+                db.task.Remove(itemToRemove);
+                db.SaveChanges();
+            }
+            return 1;
+        }
+    }
+}
---
+++ WebAPI_ToDo_final/WebAPI_Basic/Models/TaskContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI_Basic.Models
+{
+    public class TaskContext : DbContext
+    {
+        public TaskContext() :base("Name=taskContext")
+        {
+        }
+        public DbSet<Tasks> task { get; set; }
+    }
+
+    public class taskDBCreator : DropCreateDatabaseIfModelChanges<TaskContext>
+    {
+
+    }
+}
