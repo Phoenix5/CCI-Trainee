a4610b1f1b4b48268a75189191dd13312a57c692
Phoenix5 <mashelkarprathamesh@gmail.com>
2017-03-27 17:13:10 +0530

    changes

--- ToDo_WebAPI/WebAPI_Basic/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace WebAPI_Basic.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- ToDo_WebAPI/WebAPI_Basic/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace WebAPI_Basic.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
